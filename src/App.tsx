49ea6998d05fe29fbb21a698730692513da84890
Arber Shaska <arbershaska2@gmail.com>
2024-07-21 23:43:57 +0200

    Typescript is complete

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import "./App.css";
 
 function App() {
 
-  const MonthArr = ["Nothing","January","February","March","April","May","June","July","August","September","Octomber","November","December"]
+  const MonthArr = ["January","February","March","April","May","June","July","August","September","Octomber","November","December"]
 
-  const [DataUser,SetDataUser] = useState(0)
-  const [MonthUser,SetMonthUser] = useState(0)
-  const [YearUser,SetYearUser] = useState(0)
+  const [DataUser,SetDataUser] = useState<number>(0)
+  const [MonthUser,SetMonthUser] = useState<number>(0)
+  const [YearUser,SetYearUser] = useState<number>(0)
+  const [RESULTDAY,SetRESULTDAY] = useState<number>(0)
+  const [RESULTMONTH,SetRESULTMONTH] = useState<number>(0)
+  const [RESULTYEAR,SetRESULTYEAR] = useState<number>(0)
+  const [Click,setClick] = useState<Boolean>(true)
 
   const today = new Date()
   
@@ -18,41 +22,66 @@ function App() {
   const Month = today.getMonth()
   const Year = today.getFullYear()
 
-  
+ 
 
+ 
+    let ResultDay 
+    let ResultMonth
+    let ResultYear
 
   function Clicked(){
-    const ChoosenDate = new Date(DataUser + " " + MonthArr[MonthUser] + " " + YearUser)
-    console.log(ChoosenDate)
-  }
+    const ChoosenDate = new Date(DataUser + " " + MonthArr[MonthUser - 1] + " " + YearUser)
+    const NewDataUser = ChoosenDate.getDate()
+    const NewMonthUser = ChoosenDate.getMonth()
+    const NewYearUser = ChoosenDate.getFullYear()
 
+    ResultDay = Data - NewDataUser
+    ResultMonth = Month - NewMonthUser
+    ResultYear = Year - NewYearUser
+
+    if(ResultDay < 0){
+      ResultDay = Math.abs(ResultDay)
+      ResultMonth = ResultMonth - 1 
+    }
+    if(ResultMonth < 0){
+      ResultMonth = Math.abs(ResultMonth)
+      ResultMonth = 12 - ResultMonth
+      ResultYear = ResultYear - 1 
+      }
+    SetRESULTDAY(ResultDay)
+    SetRESULTMONTH(ResultMonth)
+    SetRESULTYEAR(ResultYear)
+    setClick(false)
+  }
+  console.log(Click)
+  console.log(RESULTDAY)
   return (
     <>
       <div className=" bg-slate-100 h-screen ">
         <div className="flex justify-around"> 
           <div className=" flex justify-center items-start flex-col">
             <label htmlFor="">Data</label>
-            <input type="number" onChange={e => SetDataUser(e.target.value)} name="" id="" min={1} max={31} />
+            <input required="required" type="number" onChange={e => SetDataUser(parseInt((e.target.value)))} name="" id="" min={1} max={31} />
           </div>
           <div className=" flex justify-center items-start flex-col">
             <label htmlFor="">Month</label>
-            <input type="number" name="" id="" min={1} max={12} onChange={e => SetMonthUser(e.target.value)}/>
+            <input type="number" name="" id="" min={1} max={12} onChange={e => SetMonthUser(parseInt(e.target.value))}/>
           </div>
           <div className=" flex justify-center items-start flex-col">
             <label htmlFor="">Year</label>
-            <input type="number" name="" id="" max={2024} onChange={e => SetYearUser(e.target.value)}/>
+            <input type="number" name="" id="" max={2024} onChange={e => SetYearUser(parseInt(e.target.value))}/>
           </div>
         </div>
         <button onClick={Clicked}>Click here</button>
         <div>
           <div>
-            <h1>-- Number</h1>
+            {(RESULTDAY === undefined  || Number.isNaN(RESULTDAY)) || Click ? <h1>-- days</h1>: <h1>{RESULTDAY} days</h1>}
           </div>
           <div>
-            <h1>-- Number</h1>
+          {(RESULTMONTH === undefined  || Number.isNaN(RESULTMONTH)) || Click ? <h1>-- months</h1>: <h1>{RESULTDAY} months</h1>}
           </div>
           <div>
-            <h1>-- Number</h1>
+          {(RESULTYEAR === undefined  || Number.isNaN(RESULTYEAR)) || Click ? <h1>-- years</h1>: <h1>{RESULTYEAR} years</h1>}
           </div>
         </div>
       </div>
